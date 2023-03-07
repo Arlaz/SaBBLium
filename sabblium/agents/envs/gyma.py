2b2cd3685ef072835d94c2dcf96e3dbe1c081641
Mathis Koroglu <mathis.koroglu@etu.sorbonne-universite.fr>
2023-03-07 21:41:31 +0100

    Add: VecGymAgent
    Rename GymAgent to ParallelGymAgent and create GymAgent ABC

--- sabblium/agents/envs/gyma.py
+++ sabblium/agents/envs/gyma.py
@@ -7,18 +7,31 @@
 # LICENSE file in the root directory of this source tree.
 #
 import copy
-from typing import Any, Callable, Dict, List, Optional, Union
+from abc import ABC
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    OrderedDict,
+    Sequence,
+    Union,
+)
 
-import gymnasium
 import numpy as np
 import torch
+from gymnasium import Env, Space
+from gymnasium.core import ActType, ObsType
+from gymnasium.vector import AsyncVectorEnv, SyncVectorEnv, VectorEnv
 from gymnasium.wrappers import AutoResetWrapper
-from torch import nn
+from torch import nn, Tensor
 
 from sabblium import SeedableAgent, SerializableAgent, TimeAgent
 
 
-def _convert_action(action: torch.Tensor) -> Union[int, np.ndarray[int]]:
+def _convert_action(action: Tensor) -> Union[int, np.ndarray[int]]:
     if len(action.size()) == 0:
         action = action.item()
         assert isinstance(action, int)
@@ -27,13 +40,15 @@ def _convert_action(action: torch.Tensor) -> Union[int, np.ndarray[int]]:
     return action
 
 
-def _format_frame(frame) -> Union[torch.Tensor, Dict[str, torch.Tensor]]:
-    if isinstance(frame, dict):
+def _format_frame(
+    frame: Union[Dict[str, Tensor], List[Tensor], np.ndarray, Tensor, bool, int, float]
+) -> Union[Tensor, Dict[str, Tensor]]:
+    if isinstance(frame, Dict):
         r = {}
         for k in frame:
             r[k] = _format_frame(frame[k])
         return r
-    elif isinstance(frame, list):
+    elif isinstance(frame, List):
         t = torch.tensor(frame).unsqueeze(0)
         if t.dtype == torch.float64 or t.dtype == torch.float32:
             t = t.float()
@@ -47,7 +62,7 @@ def _format_frame(frame) -> Union[torch.Tensor, Dict[str, torch.Tensor]]:
         else:
             t = t.long()
         return t
-    elif isinstance(frame, torch.Tensor):
+    elif isinstance(frame, Tensor):
         return frame.unsqueeze(0)
     elif isinstance(frame, bool):
         return torch.tensor([frame]).bool()
@@ -65,33 +80,79 @@ def _format_frame(frame) -> Union[torch.Tensor, Dict[str, torch.Tensor]]:
             assert False
 
 
-def _torch_type(d: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+def _torch_type(d: Dict[str, Tensor]) -> Dict[str, Tensor]:
     return {k: d[k].float() if torch.is_floating_point(d[k]) else d[k] for k in d}
 
 
-def _torch_cat_dict(d: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+def _torch_cat_dict(d: List[Dict[str, Tensor]]) -> Dict[str, Tensor]:
     r = {}
     for k in d[0]:
         r[k] = torch.cat([dd[k] for dd in d], dim=0)
     return r
 
 
-class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
-    """Create an Agent from a gymnasium environment
-    To create an auto-reset GymAgent, use the gymnasium `AutoResetWrapper` before creating the `GymAgent
-    """
-
+class GymAgent(TimeAgent, SeedableAgent, SerializableAgent, ABC):
     default_seed = 0
-    max_eposide_steps_replacement = 1000000
+    max_episode_steps_replacement = 1000000
 
     def __init__(
         self,
-        make_env_fn: Callable[[Optional[Dict[str, Any]]], gymnasium.Env],
-        n_envs: int,
-        make_env_args: Optional[Dict[str, Any]] = None,
         input_string: str = "action",
         output_string: str = "env/",
         max_episode_steps: Optional[int] = None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.ghost_params: nn.Parameter = nn.Parameter(torch.randn(()))
+
+        self.input: str = input_string
+        self.output: str = output_string
+        self._max_episode_steps: Optional[int] = max_episode_steps
+        self._timestep_from_reset: int = 0
+        self._nb_reset: int = 0
+
+        self.observation_space: Optional[Space[ObsType]] = None
+        self.action_space: Optional[Space[ActType]] = None
+
+    def forward(self, t: int, *args, **kwargs) -> None:
+        if t == 0:
+            self._timestep_from_reset = 0
+            self._nb_reset += 1
+        else:
+            self._timestep_from_reset += 1
+
+    def set_obs(self, observations: Dict[str, Tensor], t: int) -> None:
+        for k in observations:
+            self.set(
+                (self.output + k, t),
+                observations[k].to(self.ghost_params.device),
+            )
+
+    def get_observation_space(self) -> Space[ObsType]:
+        """Return the observation space of the environment"""
+        if self.observation_space is None:
+            raise ValueError("The observation space is not defined")
+        return self.observation_space
+
+    def get_action_space(self) -> Space[ActType]:
+        """Return the action space of the environment"""
+        if self.action_space is None:
+            raise ValueError("The action space is not defined")
+        return self.action_space
+
+
+class ParallelGymAgent(GymAgent):
+    """Create an Agent from a gymnasium environment
+    To create an auto-reset ParallelGymAgent, use the gymnasium `AutoResetWrapper` in the make_env_fn
+    """
+
+    def __init__(
+        self,
+        make_env_fn: Callable[[Optional[Dict[str, Any]]], Env],
+        num_envs: int,
+        make_env_args: Optional[Dict[str, Any]] = None,
         *args,
         **kwargs,
     ):
@@ -99,42 +160,30 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
 
         Args:
             make_env_fn ([function that returns a gymnasium.Env]): The function to create a single gymnasium environments
-            n_envs ([int]): The number of environments to create.
+            num_envs ([int]): The number of environments to create.
             make_env_args (dict): The arguments of the function that creates a gymnasium.Env
             input_string (str, optional): [the name of the action variable in the workspace]. Defaults to "action".
             output_string (str, optional): [the output prefix of the environment]. Defaults to "env/".
             max_episode_steps (int, optional): Max number of steps per episode. Defaults to None (never ends)
         """
         super().__init__(*args, **kwargs)
-        assert n_envs > 0, "n_envs must be > 0"
+        assert num_envs > 0, "n_envs must be > 0"
 
-        self.make_env_fn: Callable[
-            [Optional[Dict[str, Any]]], gymnasium.Env
-        ] = make_env_fn
-        self.env_args: Optional[Dict[str, Any]] = make_env_args
-        self.n_envs: int = n_envs
-        self.input: str = input_string
-        self.output: str = output_string
+        self.make_env_fn: Callable[[Optional[Dict[str, Any]]], Env] = make_env_fn
+        self.num_envs: int = num_envs
 
-        self.ghost_params: nn.Parameter = nn.Parameter(torch.randn(()))
-
-        self.envs: List[gymnasium.Env] = []
+        self.envs: List[Env] = []
         self.cumulated_reward: Dict[int, float] = {}
 
-        self._max_episode_steps: Optional[int] = max_episode_steps
-        self._timestep: torch.Tensor
-        self._timestep_from_reset: int = 0
+        self._timestep: Tensor
         self._is_autoreset: bool = False
         self._last_frame: Dict[int] = {}
-        self._nb_reset: int = 0
 
-        self._initialize_envs(n_envs)
+        args: Dict[str, Any] = make_env_args if make_env_args is not None else {}
+        self._initialize_envs(num_envs=num_envs, make_env_args=args)
 
-    def _initialize_envs(self, n):
-        if self.env_args is None:
-            self.envs = [self.make_env_fn() for _ in range(n)]
-        else:
-            self.envs = [self.make_env_fn(**self.env_args) for _ in range(n)]
+    def _initialize_envs(self, num_envs, make_env_args: Dict[str, Any]):
+        self.envs = [self.make_env_fn(**make_env_args) for _ in range(num_envs)]
         self._timestep = torch.zeros(len(self.envs), dtype=torch.long)
         self.observation_space = self.envs[0].observation_space
         self.action_space = self.envs[0].action_space
@@ -148,31 +197,31 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
 
         if self._is_autoreset and self._max_episode_steps is None:
             raise ValueError(
-                "AutoResetWrapper without max_episode_steps argument given will never"
+                "AutoResetWrapper without max_episode_steps argument given will (probably) never"
                 "stop the GymAgent if wrapped with a TemporalAgent"
             )
 
-    def _reset(self, k: int, render: bool) -> Dict[str, torch.Tensor]:
-        env: gymnasium.Env = self.envs[k]
+    def _reset(self, k: int) -> Dict[str, Tensor]:
+        env: Env = self.envs[k]
         self.cumulated_reward[k] = 0.0
 
         s: int = (
             (
                 self._max_episode_steps
                 if self._max_episode_steps is not None
-                else self.max_eposide_steps_replacement
+                else self.max_episode_steps_replacement
             )
-            * self.n_envs
+            * self.num_envs
             * self._nb_reset
             * self._seed
         )
         s += (k + 1) * (self._timestep[k].item() + 1 if self._is_autoreset else 1)
         o, info = env.reset(seed=s)
-        observation: Union[torch.Tensor, Dict[str, torch.Tensor]] = _format_frame(o)
+        observation: Union[Tensor, Dict[str, Tensor]] = _format_frame(o)
 
         self._timestep[k] = 0
 
-        if isinstance(observation, torch.Tensor):
+        if isinstance(observation, Tensor):
             observation = {"env_obs": observation}
         elif isinstance(observation, dict):
             pass
@@ -180,10 +229,8 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
             raise ValueError(
                 f"Observation must be a torch.Tensor or a dict, not {type(observation)}"
             )
-        if render:
-            observation["rendering"] = env.render().unsqueeze(0)
 
-        ret: Dict[str, torch.Tensor] = {
+        ret: Dict[str, Tensor] = {
             **observation,
             "terminated": torch.tensor([False]),
             "truncated": torch.tensor([False]),
@@ -195,16 +242,16 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
         self._last_frame[k] = ret
         return _torch_type(ret)
 
-    def _step(self, k: int, action: Union[int, np.ndarray[int]], render: bool):
+    def _step(self, k: int, action: Tensor):
         env = self.envs[k]
         action: Union[int, np.ndarray[int]] = _convert_action(action)
 
         obs, reward, terminated, truncated, info = env.step(action)
 
         self.cumulated_reward[k] += reward
-        observation: Union[torch.Tensor, Dict[str, torch.Tensor]] = _format_frame(obs)
+        observation: Union[Tensor, Dict[str, Tensor]] = _format_frame(obs)
 
-        if isinstance(observation, torch.Tensor):
+        if isinstance(observation, Tensor):
             observation = {"env_obs": observation}
         elif isinstance(observation, dict):
             pass
@@ -212,8 +259,6 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
             raise ValueError(
                 f"Observation must be a torch.Tensor or a dict, not {type(observation)}"
             )
-        if render:
-            observation["rendering"] = env.render().unsqueeze(0)
 
         self._timestep[k] += 1
 
@@ -225,7 +270,7 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
         else:
             stopped = terminated or truncated
 
-        ret: Dict[str, torch.Tensor] = {
+        ret: Dict[str, Tensor] = {
             **observation,
             "terminated": torch.tensor([terminated]),
             "truncated": torch.tensor([truncated]),
@@ -237,51 +282,91 @@ class GymAgent(TimeAgent, SeedableAgent, SerializableAgent):
         self._last_frame[k] = ret
         return _torch_type(ret)
 
-    def set_obs(self, observations: List[Dict[str, torch.Tensor]], t: int) -> None:
-        observations: Dict[str, torch.Tensor] = _torch_cat_dict(observations)
-        for k in observations:
-            self.set(
-                (self.output + k, t),
-                observations[k].to(self.ghost_params.device),
-            )
-
-    def forward(self, t: int = 0, render: bool = False, **kwargs):
+    def forward(self, t: int = 0, **kwargs) -> None:
         """Do one step by reading the `action` at t-1
         If t==0, environments are reset
         If render is True, then the output of env.render() is written as env/rendering
         """
-
-        self._timestep_from_reset += 1
+        super().forward(t, **kwargs)
 
         observations = []
         if t == 0:
-            self._timestep_from_reset = 0
-            self._nb_reset += 1
             if self._seed is None:
                 self.seed(self.default_seed)
             for k, env in enumerate(self.envs):
-                observations.append(self._reset(k, render))
+                observations.append(self._reset(k))
         else:
             action = self.get((self.input, t - 1))
-            assert action.size()[0] == self.n_envs, "Incompatible number of envs"
+            assert action.size()[0] == self.num_envs, "Incompatible number of envs"
 
             for k, env in enumerate(self.envs):
                 if self._is_autoreset or not self._last_frame[k]["stopped"]:
-                    observations.append(self._step(k, action[k], render))
+                    observations.append(self._step(k, action[k]))
                 else:
                     observations.append(self._last_frame[k])
-        self.set_obs(observations, t)
-
-    def get_observation_space(self):
-        """Return the observation space of the environment"""
-        return self.observation_space
-
-    def get_action_space(self):
-        """Return the action space of the environment"""
-        return self.action_space
+        self.set_obs(observations=_torch_cat_dict(observations), t=t)
 
 
-class ImageGymAgent(GymAgent, SerializableAgent):
+class VecGymAgent(GymAgent):
+    def __init__(
+        self,
+        make_envs_fn: Callable[[Optional[Dict[str, Any]]], VectorEnv],
+        vec_env_args: Optional[Dict[str, Any]] = None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        args: Dict[str, Any] = vec_env_args or {}
+        self.envs: VectorEnv = make_envs_fn(**args)
+
+        self.observation_space = self.envs.single_observation_space
+        self.action_space = self.envs.single_action_space
+
+        self.cumulated_reward: Tensor = torch.zeros(self.envs.num_envs)
+
+    def forward(self, t: int, **kwargs) -> None:
+        super().forward(t, **kwargs)
+
+        if t == 0:
+            obs, infos = self.envs.reset(seed=self._seed)
+            termination = torch.tensor([False] * self.envs.num_envs)
+            truncation = torch.tensor([False] * self.envs.num_envs)
+            rewards = torch.tensor([0.0] * self.envs.num_envs)
+            self.cumulated_reward = torch.zeros(self.envs.num_envs)
+            stopped = False
+        else:
+            action = self.get((self.input, t - 1))
+            assert (
+                action.size()[0] == self.envs.num_envs
+            ), "Incompatible number of actions"
+            converted_action: Union[int, np.ndarray[int]] = _convert_action(action)
+            obs, rewards, termination, truncation, infos = self.envs.step(
+                converted_action
+            )
+            rewards = torch.tensor(rewards).float()
+            termination = torch.tensor(termination)
+            truncation = torch.tensor(truncation)
+            self.cumulated_reward = self.cumulated_reward + rewards
+            stopped = self._timestep_from_reset + 1 >= self._max_episode_steps
+
+        observation: Union[Tensor, Dict[str, Tensor]] = _format_frame(obs)
+
+        if not isinstance(observation, Tensor):
+            raise ValueError("Observation can't be an OrderedDict in a VecEnv")
+
+        ret: Dict[str, Tensor] = {
+            "env_obs": observation.squeeze(0),
+            "terminated": termination,
+            "truncated": truncation,
+            "stopped": torch.tensor([stopped]),
+            "reward": rewards,
+            "cumulated_reward": self.cumulated_reward,
+        }
+        self.set_obs(observations=ret, t=t)
+
+
+class ImageGymAgent(GymAgent, ABC):
     """
     GymAgent compatible with image observations
     """
@@ -294,3 +379,13 @@ class ImageGymAgent(GymAgent, SerializableAgent):
         copied_agent = copy.copy(self)
         copied_agent.envs = None
         return copied_agent
+
+
+class VecImageGymAgent(VecGymAgent, ImageGymAgent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class ParallelImageGymAgent(ParallelGymAgent, ImageGymAgent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
